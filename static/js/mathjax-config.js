25ca293b2a4ded3c0fbea3c18f836108bd478b02
Jerin Philip <jerin.philip@research.iiit.ac.in>
2019-09-21 22:11:39 +0530

    [ci skip] Update]

--- static/js/mathjax-config.js
+++ static/js/mathjax-config.js
@@ -4,6 +4,11 @@ var config = {
             ['$','$'], 
             ['\\(','\\)']
         ]
+    },
+    chtml: {
+        scale: 1,
+        matchFontHeight: true,         // true to match ex-height of surrounding font
+        mathmlSpacing: true          // true for MathML spacing rules, false for TeX rules
     }
 }
 MathJax.Hub.Config(config);
